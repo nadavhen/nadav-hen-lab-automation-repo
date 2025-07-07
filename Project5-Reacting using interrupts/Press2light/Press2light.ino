73870c94352374367251e80e639621c73ccf4689
nadavhen <107586157+nadavhen@users.noreply.github.com>
2025-07-07 11:07:50 +0300

    ex5 solution

--- Project5-Reacting using interrupts/Press2light/Press2light.ino
+++ Project5-Reacting using interrupts/Press2light/Press2light.ino
@@ -1,28 +1,42 @@
 #define LED_PIN 4
 #define BUTTON_PIN 6
+#define INTERRUPT_PIN 2 // Interrupt pin (must be pin 2 or 3 on most Arduino boards)
+
+volatile bool buttonPressed = false; // Flag to track button state
+volatile unsigned long lastInterruptTime = 0; // Timestamp of the last interrupt
+
+void handleInterrupt() {
+  // Interrupt service routine (ISR)
+  unsigned long currentTime = millis(); // Get the current time
+  if (currentTime - lastInterruptTime > 50) { // Check if 50ms have passed since the last interrupt
+    buttonPressed = digitalRead(BUTTON_PIN); // Read the button state directly
+    if (buttonPressed) {
+      digitalWrite(LED_PIN, HIGH); // Turn the LED on
+      Serial.println("Button pressed: LED ON");
+    } else {
+      digitalWrite(LED_PIN, LOW); // Turn the LED off
+      Serial.println("Button released: LED OFF");
+    }
+    lastInterruptTime = currentTime; // Update the last interrupt time
+  }
+}
+
 void setup() {
   // Initialize the Serial communication
   Serial.begin(9600);
 
   // Set pin modes
-  pinMode(BUTTON_PIN, INPUT); // Button input
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Button input with internal pull-up
   pinMode(LED_PIN, OUTPUT); // LED output
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP); // Interrupt pin input (connected to BUTTON_PIN)
+
+  // Attach interrupt to INTERRUPT_PIN
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), handleInterrupt, CHANGE);
+  // The interrupt triggers on any change (HIGH to LOW or LOW to HIGH) on INTERRUPT_PIN
 }
 
 void loop() {
-  // Read the state of the input pin (pin 6)
-  int buttonState = digitalRead(BUTTON_PIN);
-
-  // Check if the button is pressed (HIGH state)
-  if (buttonState == HIGH) {
-    digitalWrite(LED_PIN, HIGH); // Turn the LED on
-    Serial.println("Button pressed: LED ON"); // Debug message for button press
-  } else {
-    digitalWrite(LED_PIN, LOW); // Turn the LED off
-    Serial.println("Button released: LED OFF"); // Debug message for button release
+  for (int i = 0; i< 10000; i++){
+      Serial.println("calculating...");
   }
-
-  for (int i = 0; i< 1000; i++){
-    Serial.println("calculating...");
-}
 }
